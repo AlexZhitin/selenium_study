a73d87f5c7728179fc9a612a49cdb4b6b78a0854
SashaZ <aszhitin@gmail.com>
2018-11-28 17:43:45 +0200

    @Before@Test@After annotations
    -Launching a few tests one by one

--- src/test/java/ua/selenium/study/FirstTestChrome.java
+++ src/test/java/ua/selenium/study/FirstTestChrome.java
@@ -1,16 +1,43 @@
 package ua.selenium.study;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Dimension;
 
 public class FirstTestChrome {
 
-  public static void main(String[] args) {
-    WebDriver driver = new ChromeDriver();
+
+  ChromeDriver driver = new ChromeDriver();
+
+  @Before
+
+          public void setUp (){
     System.setProperty("webdriver.chrome.driver", "/Users/Sasha/Documents/Selenium projects/selenium_study/src/main/resources/chromedriver");
+    System.out.println("Before of test");
+
+  }
+
+  @Test
+  public void Startoftest1() {
+
     driver.get("https://en.wikipedia.org/wiki/Selenium_(software)");
+    System.out.println("Start of test");
+  }
+
+  @Test
+
+  public void Startoftest2() {
+
+    driver.get("https://en.wikipedia.org/wiki/Selenium_(software)");
+    System.out.println("Start of test 2");
+  }
+
+  @After
+
+  public void End() {
+    driver.quit();
+    System.out.println("End of test");
   }
 }
