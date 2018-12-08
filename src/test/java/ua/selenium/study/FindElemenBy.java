a6d5ef34d398e1eefdea13974791b32e5c188da6
AlexZhitin <38076824+AlexZhitin@users.noreply.github.com>
2018-12-08 22:45:20 +0200

    No changes

--- src/test/java/ua/selenium/study/FindElemenBy.java
+++ src/test/java/ua/selenium/study/FindElemenBy.java
@@ -8,16 +8,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElemenBy {
 
-  ChromeDriver driver = new ChromeDriver();
+    ChromeDriver driver = new ChromeDriver();
 
-  @Test
+    @Test
 
-  public void findElementBy() {
-    driver.get("https://www.w3schools.com/");
-    driver.findElement(By.className("w3-right"));
-    driver.findElement(By.id("___gcse_0"));
-    //driver.findElementByLinkText("Learn CSS").click();
-    //driver.findElement(By.className("gsc-search-button-v2")).click();
-    driver.findElement(By.xpath("/html/body/div[5]/div/a[1]/i")).click();
+    public void findElementBy() {
+
+      driver.get("https://www.w3schools.com/");
+      driver.findElement(By.className("w3-right"));
+      driver.findElement(By.id("___gcse_0"));
+      //driver.findElementByLinkText("Learn CSS").click();
+      //driver.findElement(By.className("gsc-search-button-v2")).click();
+      driver.findElement(By.xpath("/html/body/div[5]/div/a[1]/i")).click();
+    }
   }
-}
