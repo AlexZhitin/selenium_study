ecfac9956572cde4e4938b6f3f5bcabf2709e4f2
AlexZhitin <38076824+AlexZhitin@users.noreply.github.com>
2018-12-09 19:15:20 +0200

    FindElement and Input

--- src/test/java/ua/selenium/study/FindElemenBy.java
+++ src/test/java/ua/selenium/study/FindElemenBy.java
@@ -15,10 +15,13 @@ public class FindElemenBy {
     public void findElementBy() {
 
       driver.get("https://www.w3schools.com/");
-      driver.findElement(By.className("w3-right"));
-      driver.findElement(By.id("___gcse_0"));
+      //driver.findElement(By.className("w3-right"));
+      //driver.findElement(By.id("___gcse_0"));
       //driver.findElementByLinkText("Learn CSS").click();
       //driver.findElement(By.className("gsc-search-button-v2")).click();
-      driver.findElement(By.xpath("/html/body/div[5]/div/a[1]/i")).click();
+      //driver.findElement(By.xpath("/html/body/div[5]/div/a[1]/i")).click();
+      driver.findElement(By.xpath("/html/body/div[5]/div/a[@title='Search W3Schools']/i[1]")).click();
+      driver.findElement(By.xpath("//*[@id=\"gsc-i-id1\"][@placeholder='Custom Search']")).sendKeys("ededed");
+      driver.findElement(By.xpath("//*[@id=\"___gcse_0\"]/div/div/form/table/tbody/tr/td[2]/button")).click();
     }
   }
