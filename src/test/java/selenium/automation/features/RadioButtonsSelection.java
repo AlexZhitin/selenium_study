9ba6724306860aea3d600054b624429544abb331
SashaZ <aszhitin@gmail.com>
2018-12-12 18:56:44 +0200

    RadioButtonSelection

--- src/test/java/selenium/automation/features/RadioButtonsSelection.java
+++ src/test/java/selenium/automation/features/RadioButtonsSelection.java
@@ -1,5 +1,6 @@
 package selenium.automation.features;
 
+import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,23 +9,37 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.awt.*;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class RadioButtonsSelection {
+
 
     WebDriver driver = new ChromeDriver();
 
     @Test
 
-    public void RadioButtons(){
+    public void RadioButtons() {
 
         driver.get("https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_radio");
-        List<WebElement> list = driver.findElements(By.xpath("/html[1]/body[1]/form[1]/input[@name='gender']"));
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        List<WebElement> list = driver.findElements(By.xpath("/html[1]/body[1]/form[1]/input[1]"));
 
-        for(WebElement e: list){
-            System.out.println(e.getAttribute("Male"));
+        System.out.println(list.size());
+
+        for (WebElement e : list) {
+            System.out.println(e.getAttribute("value"));
             System.out.println(e.isSelected());
+
+            if (e.getAttribute("value").equals("other")){
+                e.click();
+            }
         }
 
+        System.out.println("________________");
+                for (WebElement e : list) {
+                    System.out.println(e.getAttribute("value"));
+                    System.out.println(e.isSelected());
 
-    }
-}
+                }
+            }
+        }
